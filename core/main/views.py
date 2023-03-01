9ea35f379384c96f2f08f9e0248f25edc968c4e3
SamvelKurghinyan <samvel.kurghinyan.hh@gmail.com>
2023-03-01 14:52:09 +0400

    search

---
+++ core/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+from django.db.models import Q
+
+def index(request):
+
+    search_post = request.GET.get('search')
+
+    if search_post:    
+        posts = Post.objects.filter(Q(title__icontains=search_post) | Q(content__icontains=search_post))
+    else:    
+        posts = Post.objects.all().order_by("-date_created")
+    return render(request, 'main/index.html', {'posts': posts})
